9fc94a2410e252bc33bececf6612096d5758e39b
pasha1019 <84281385+pasha1019@users.noreply.github.com>
2022-06-25 19:59:16 +0300

    Generating a password and copying it to the clipboard

--- passW.py
+++ passW.py
@@ -1,23 +1,21 @@
 # Генерация пароля и передача его в буфер обмена
+import secrets
 import random
+import string
 import pyperclip
 
+list_symbol = string.ascii_letters + string.digits
+passW = ''
 print("Выберите из списка сложность пароля")
 print('1. Простой пароль\n'
       '2. Средняя стойкость\n'
       '3. Высокая стойкость\n')
-level_pass = input() # Ввод уровня сложности
-passW=''
-chars1 = 'abcdefghijklnopqrstuvwxyz' \
-         'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-chars2 = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyz' \
-         'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+level_pass = input()
 if int(level_pass) == 1:
     for i in range(8):
         passW += str(random.randint(0,9)) # Генерация простого пароля
 elif int(level_pass) == 2:
-    for i in range(8):
-        passW += random.choice(chars1) # Генерация пароля средней сложности
+    passW = ''.join(secrets.choice(list_symbol) for i in range(9))
 elif int(level_pass) == 3:
     while True:
         print('Уточните длину пароля:')
@@ -25,11 +23,17 @@ elif int(level_pass) == 3:
         try: # Защита от "дурака"
             if int(length) < 9:
                 print('Пароль должен содержать минимум 9 символов')
+            elif int(length) > 99:
+                print('Зачем Вам такой длинный пароль?')
             else:
                 break
         except:
             print('Введите число')
-    for i in range(int(length)):
-        passW += random.choice(chars2) # Генерация пароля высокой сложности
+    while True:
+        passW = ''.join(secrets.choice(list_symbol) for i in range(int(length)))
+        if (any(c.islower() for c in passW)
+            and any(c.isupper() for c in passW)
+            and sum(c.isdigit() for c in passW) >= 3):
+                break
 print(passW)
 pyperclip.copy(passW)
